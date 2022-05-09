ed323cde1370afd14bf6308e6b5010b63f6d016f
Jordan Foote <82241562+jordanfoote@users.noreply.github.com>
2022-05-08 19:33:57 -0500

    Completed Subqueries Exercises

---
+++ subqueries_exercises.sql
@@ -0,0 +1,23 @@
+SELECT * FROM employees
+WHERE hire_date = (
+    SELECT hire_date
+    FROM employees
+    WHERE emp_no = '101010'
+    );
+
+SELECT DISTINCT title
+FROM titles
+WHERE emp_no IN (
+    SELECT emp_no
+    FROM employees
+    WHERE first_name = 'Aamod'
+    );
+
+SELECT first_name, last_name
+FROM employees
+WHERE emp_no IN (
+    SELECT emp_no
+    FROM dept_manager
+    WHERE to_date LIKE '9999%'
+    AND gender = 'F'
+    );
